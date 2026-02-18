0e189acdc7fb59b681dc611531954a678d7da5b6
timpamungkas <timotius.pamungkas@gmail.com>
2026-02-18 13:22:46 +0700

    hello

--- src/main/java/com/course/grpcserver/grpc/server/HelloServiceGrpcController.java
+++ src/main/java/com/course/grpcserver/grpc/server/HelloServiceGrpcController.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Service;
 
 import com.course.central.proto.hello.Hello.SayHelloRequest;
 import com.course.central.proto.hello.Hello.SayHelloResponse;
-import com.course.grpcserver.logic.HelloLogic;
+import com.course.grpcserver.service.HelloService;
 import com.course.central.proto.hello.HelloServiceGrpc;
 
 import io.grpc.stub.StreamObserver;
@@ -13,16 +13,16 @@ import io.grpc.stub.StreamObserver;
 @Service
 public class HelloServiceGrpcController extends HelloServiceGrpc.HelloServiceImplBase {
 
-    private HelloLogic helloLogic;
+    private HelloService helloService;
 
-    public HelloServiceGrpcController(@Autowired HelloLogic helloLogic) {
-        this.helloLogic = helloLogic;
+    public HelloServiceGrpcController(@Autowired HelloService helloService) {
+        this.helloService = helloService;
     }
 
     @Override
     public void sayHello(SayHelloRequest request, StreamObserver<SayHelloResponse> responseObserver) {
         var name = request.getName();
-        var message = helloLogic.generateHello(name);
+        var message = helloService.generateHello(name);
 
         var response = SayHelloResponse.newBuilder()
                 .setGreet(message)
